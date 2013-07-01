d382591fa96605289cdc5810a789522414f17325
Daniel Saul <daniel@dansaul.co.uk>
2013-07-01 17:17:11 +0100

    Changed to twython, uses logging module

--- moodmap/twitter.py
+++ moodmap/twitter.py
@@ -1,34 +1,43 @@
 from .utils import *
-import tweepy
+from twython import TwythonStreamer
+import logging
 
-auth = tweepy.auth.BasicAuthHandler(env("TWITTER_USERNAME"),
-        env("TWITTER_PASSWORD"))
+logger = logging.getLogger('moodmap.twitter')
 
-locations = [
-    -7.93, 55.40, -0.53, 60.92,
-    -5.65, 51.60, 2.33, 56.13,
-    -5.52, 50.60, 1.67, 51.84,
-    -6.64, 49.82, 1.54, 51.29, 
-    -8.28, 54.07, -5.27, 55.35
-]
+# UK Location Boundary
+#locations = [
+#    -7.93, 55.40, -0.53, 60.92,
+#    -5.65, 51.60, 2.33, 56.13,
+#    -5.52, 50.60, 1.67, 51.84,
+#    -6.64, 49.82, 1.54, 51.29, 
+#    -8.28, 54.07, -5.27, 55.35
+#]
 
 callback = None
 
-class StreamWatcherListener(tweepy.StreamListener):
-    def __init__(self, callback, api=None):
+class Streamer(TwythonStreamer):
+    def set_callback(self, callback):
         self.callback = callback
-        super(StreamWatcherListener, self).__init__(api)
 
-    def on_status(self, status):
-        self.callback(status)
+    def on_success(self, data):
+        #logger.debug(data)
+        if self.callback:
+            self.callback(data)
 
-    def on_error(self, status_code):
-        log("Twitter Error: {0}".format(status_code))
-        return True
-
+    def on_error(self, status_code, data):
+        logger.warning("Twitter Error: [{0}] {1}".format(status_code, data['text']))
+    
     def on_timeout(self):
-        log("Twitter Snoozing")
+        logger.warning("Twitter Snoozing")
 
-def start(callback):
-    stream = tweepy.Stream(auth, StreamWatcherListener(callback))
-    stream.filter(locations=locations)
+def start(filter_location, locations=locations, callback=callback):
+    stream = Streamer(env("TWITTER_CONSUMER_KEY"), env("TWITTER_CONSUMER_SECRET"), env("TWITTER_ACCESS_TOKEN"), env("TWITTER_ACCESS_TOKEN_SECRET"))
+    stream.set_callback(callback)
+
+    logger.info("Twitter stream starting.")
+
+    if filter_location:
+        stream.statuses.filter(locations=locations)
+    else:
+        stream.statuses.sample(language="en")
+
